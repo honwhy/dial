111541e982280d9d821a3332afd1e576bdb119cb
Honwhy Wang <honwhy.wang@gmail.com>
2019-12-14 10:43:54 +0800

    add timeout feature

--- src/main.rs
+++ src/main.rs
@@ -12,17 +12,18 @@ use structopt::StructOpt;
 #[structopt(name = "dail example", about = "An example of server ports scanning.")]
 struct Opt {
     #[structopt(long = "hostname", default_value = "", help = "hostname to test")]
-    host_name: String,
+    hostname: String,
     #[structopt(long = "start-port", default_value = "80", help = "the port on which the scanning starts" )]
     start_port: u16,
     #[structopt(long = "end-port", default_value = "100", help = "the port from which the scanning ends" )]
     end_port: u16,
     #[structopt(long = "timeout", default_value = "200", help = "timeout" )]
-    timeout: u32,
+    timeout: u64,
 
 }
 fn main() {
     let opt = Opt::from_args();
+    let timeout = Duration::from_millis(opt.timeout);
     let vec: Vec<u16> = Vec::new();
     let arc_vec = Arc::new(Mutex::new(vec));
     
@@ -32,9 +33,9 @@ fn main() {
         // create another reference to the wait group.
         let wg = wg.clone();
         let arc_vec = arc_vec.clone();
-        let domain = String::from(&opt.host_name);
+        let hostname = String::from(&opt.hostname);
         go!(move || {
-            let flag = is_open(&domain, n);
+            let flag = is_open(hostname, n, timeout);
             if flag {
                 arc_vec.lock().unwrap().push(n);
             }
@@ -48,14 +49,13 @@ fn main() {
     println!("opened ports: {:?}", arc_vec.lock().unwrap());
 }
 
-fn is_open(domain: &str, port: u16) -> bool {
-    let host = format!("{}:{}", domain, port);
-    let addrs: Vec<_> = host 
+fn is_open(hostname: String, port: u16, timeout: Duration) -> bool {
+    let server = format!("{}:{}", hostname, port);
+    let addrs: Vec<_> = server 
                             .to_socket_addrs()
                             .expect("Unable to parse socket address")
                             .collect();
 
-    let timeout = Duration::from_millis(200);
     if let Ok(stream) = TcpStream::connect_timeout(&addrs[0], timeout) {
         //println!("Connected to the server!");
         stream.shutdown(Shutdown::Both).expect("shutdown call failed");
